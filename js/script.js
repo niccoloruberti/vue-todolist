33e59bb709d7fc218ff9fc66fc1a4de8d0af4a8b
niccoloruberti <nikiruberti@gmail.com>
2023-06-09 16:45:16 +0200

    inseriti task nel DOM con js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            tasks: [
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'lavare la macchina',
+                    done: false
+                },
+                {
+                    text: 'stendere i panni',
+                    done: false
+                }
+            ]
+        }
+    }
+}).mount('#app');
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 CONSEGNA: Rifare l'esercizio della to do list visto a lezione. Create un array di oggetti ( con proprietà text e done )che rappresentano le task e mostratele a schermo. Aggiungete poi le funzionalità di cambio dello stato del task (cambiate il valore di done), eliminazione di una task ed aggiunta di una nuova task.
 
 - creo la struttura della pagina;
+- creo un array di oggetti per i task;
+- utilizzando un v-for ciclo gli oggetti dell'array per scrivere i task nel DOM;
